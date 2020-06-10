e528f40d70ff521b306448fa5d907c98d0c82ca3
Bwaris94 <54705280+Bwaris94@users.noreply.github.com>
2020-06-10 14:16:04 +0000

    Deploying to gh-pages from  @ 7e9f72b6a21b1671ab097714e81d0fae71249ae6 🚀

--- bundle/codedoc-bundle.entry.js
+++ bundle/codedoc-bundle.entry.js
@@ -16,16 +16,16 @@ import { postNavSearch } from '/home/runner/work/justblogit/justblogit/.codedoc/
 import { ToCToggle } from '/home/runner/work/justblogit/justblogit/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toggle/index.js';
 import { DarkModeSwitch } from '/home/runner/work/justblogit/justblogit/.codedoc/node_modules/@codedoc/core/dist/es6/components/darkmode/index.js';
 import { ConfigTransport } from '/home/runner/work/justblogit/justblogit/.codedoc/node_modules/@codedoc/core/dist/es6/transport/config.js';
-import { ArticleCard } from '/home/runner/work/justblogit/justblogit/.codedoc/node_modules/@codedoc/coding-blog-plugin/dist/es5/components/article-card/index.js';
 import { Author } from '/home/runner/work/justblogit/justblogit/.codedoc/node_modules/@codedoc/coding-blog-plugin/dist/es5/components/author/index.js';
+import { ArticleCard } from '/home/runner/work/justblogit/justblogit/.codedoc/node_modules/@codedoc/coding-blog-plugin/dist/es5/components/article-card/index.js';
 import { ToCPrevNext } from '/home/runner/work/justblogit/justblogit/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/prevnext/index.js';
 
 const components = {
   'b90Lyiy/r0FOJ4bsTmLr5g==': ToCToggle,
   '1j3vP8EkgI5f9pDF56oPaA==': DarkModeSwitch,
   'eY88GvYbNbq7xROVKGaVxA==': ConfigTransport,
-  'TkM91Gn8T3BiI3jHNdfS0A==': ArticleCard,
   'oTf7E4Mh/IsG1gY4REN1bA==': Author,
+  'TkM91Gn8T3BiI3jHNdfS0A==': ArticleCard,
   'iponTzOHa/HScr9XGS25zA==': ToCPrevNext
 };
 
