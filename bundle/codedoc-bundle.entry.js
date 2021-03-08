bf80d9defe25c9354d633f63009af601bba46add
Bwaris94 <54705280+Bwaris94@users.noreply.github.com>
2021-03-08 16:09:36 +0000

    Deploying to gh-pages from  @ 554c59e9d833b020ce5fc38f4485a8aaad37aed8 🚀

--- bundle/codedoc-bundle.entry.js
+++ bundle/codedoc-bundle.entry.js
@@ -13,18 +13,18 @@ import { loadDeferredIFrames } from '/home/runner/work/justblogit/justblogit/.co
 import { smoothLoading } from '/home/runner/work/justblogit/justblogit/.codedoc/node_modules/@codedoc/core/dist/es6/transport/smooth-loading.js';smoothLoading();
 import { tocHighlight } from '/home/runner/work/justblogit/justblogit/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toc-highlight.js';tocHighlight();
 import { postNavSearch } from '/home/runner/work/justblogit/justblogit/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/search/post-nav/index.js';postNavSearch();
+import { Author } from '/home/runner/work/justblogit/justblogit/.codedoc/node_modules/@codedoc/coding-blog-plugin/dist/es5/components/author/index.js';
 import { ToCToggle } from '/home/runner/work/justblogit/justblogit/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toggle/index.js';
 import { DarkModeSwitch } from '/home/runner/work/justblogit/justblogit/.codedoc/node_modules/@codedoc/core/dist/es6/components/darkmode/index.js';
 import { ConfigTransport } from '/home/runner/work/justblogit/justblogit/.codedoc/node_modules/@codedoc/core/dist/es6/transport/config.js';
-import { Author } from '/home/runner/work/justblogit/justblogit/.codedoc/node_modules/@codedoc/coding-blog-plugin/dist/es5/components/author/index.js';
 import { ArticleCard } from '/home/runner/work/justblogit/justblogit/.codedoc/node_modules/@codedoc/coding-blog-plugin/dist/es5/components/article-card/index.js';
 import { ToCPrevNext } from '/home/runner/work/justblogit/justblogit/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/prevnext/index.js';
 
 const components = {
+  'oTf7E4Mh/IsG1gY4REN1bA==': Author,
   'b90Lyiy/r0FOJ4bsTmLr5g==': ToCToggle,
   '1j3vP8EkgI5f9pDF56oPaA==': DarkModeSwitch,
   'eY88GvYbNbq7xROVKGaVxA==': ConfigTransport,
-  'oTf7E4Mh/IsG1gY4REN1bA==': Author,
   'TkM91Gn8T3BiI3jHNdfS0A==': ArticleCard,
   'iponTzOHa/HScr9XGS25zA==': ToCPrevNext
 };
